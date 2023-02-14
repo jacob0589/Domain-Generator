78aa03c549a52d077f2ec40d057c67f85c70ef73
jacob0589 <alonsocb8995@gmail.com>
2023-02-14 04:53:24 +0000

    tercer avance

--- src/app.js
+++ src/app.js
@@ -5,31 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//write your code here
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let extension = [".com", ".org"];
+function createDomain() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".org"];
 
-const ejecutar = () => {
-  let text;
-  let paragraph;
+  let domainNames = "";
+
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.lenth; k++) {
-        for (let l = 0; l < extension.lenth; l++) {
-          paragraph = document.createElement("p");
-          text = document.createTextNode(
-            pronoun[i] + adj[j] + noun[k] + extension[l]
-          );
-          paragraph.appendChild(text);
-          document.querySelector("#domainList").appendChild(paragraph);
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          domainNames += pronoun[i] + adj[j] + noun[k] + extension[l] + "<br>";
         }
       }
     }
   }
-};
+  document.getElementById("domainList").innerHTML += domainNames;
+}

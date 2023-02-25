9c77d9412e1975588c86cccb8f898e82cb68b81a
jacob0589 <alonsocb8995@gmail.com>
2023-02-25 04:09:09 +0000

    final avance

--- src/app.js
+++ src/app.js
@@ -5,24 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//window.onload = function() {
-//write your code here
+window.onload = function() {
+  document.querySelector("button").addEventListener("click", createDomain);
+};
 
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let extension = [".com", ".org"];
 
-let domainNames = "";
 function createDomain() {
+  let domainNames;
+  let paragraph;
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < extension.length; l++) {
-          domainNames += pronoun[i] + adj[j] + noun[k] + extension[l] + "<br>";
+          paragraph = document.createElement("p");
+          domainNames = document.createTextNode(
+            pronoun[i] + adj[j] + noun[k] + extension[l] + "<br>"
+          );
+          paragraph.appendChild(domainNames);
+          document.querySelector("#domainList").appendChild(paragraph);
         }
       }
     }
   }
-  document.getElementById("domainList").innerHTML = createDomain(domainNames);
 }
